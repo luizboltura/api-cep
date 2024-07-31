4f700c99ed66051d79d8737fd94e282f8e926ccc
luizboltura <fibol.dev@gmail.com>
2024-07-31 17:48:54 -0300

    Adicionado um loop para consultar mais de um CEP

--- app_cep.py
+++ app_cep.py
@@ -1,15 +1,22 @@
 import requests as rq
 
 lista_ceps = list()
-print("Digite o CEP que deseja buscar: ")
+
+print("Digite os CEPs para adicionar à lista. Digite 'sair' para parar.\n")
+
+while True:
+    cep = input("Digite um CEP para adicionar à lista: ")
+    if cep.lower() == 'sair':
+        break
+    lista_ceps.append(cep)
 
 lista_enderecos = list()
 
-cep = input("Digite um CEP para consulta: ")
+for cep in lista_ceps:
+    url = f'https://viacep.com.br/ws/{cep}/json/'
+    req = rq.get(url, timeout=3)
+    endereco = req.json()
 
-url = f'https://viacep.com.br/ws/{cep}/json/'
-req = rq.get(url, timeout=3)
-endereco = req.json()
+    lista_enderecos.append(endereco)
 
-
-print(endereco)
+print(lista_enderecos)
